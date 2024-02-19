58f280ce43948846eb387763b803cad92db232ca
Alice Zhao <alice@zhaos.dev>
2024-02-18 20:50:02 -0800

    extract test expected cards into its own function and write turbo test

--- src/tests/unlockTests.ts
+++ src/tests/unlockTests.ts
@@ -1,10 +1,15 @@
-import { RestaurantSettings, Unlocks } from "../workers/db/unlocks";
+import {
+	RestaurantSettings,
+	StartingDishes,
+	Unlocks,
+} from "../workers/db/unlocks";
 import { FindNewUnlocks } from "../workers/reverse-engineered/cards";
-
+import { Unlock } from "../kitchenTypes";
+const normalCardDays = [3, 5, 6, 9, 12, 15];
+const turboCardDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 export function test(): boolean {
-	const a = new FindNewUnlocks("azqyj4nh");
-	a.addCard(RestaurantSettings.filter((a) => a.Name === "Community")[0]); // Autumn Setting
-	const expectedCards = [
+	const seed = "azqyj4nh";
+	const expectedCards: [string, string][] = [
 		["Black Coffee", "Ice Cream"],
 		["Affordable", "Charming"],
 		["Christmas Crackers", "Pizza"],
@@ -12,33 +17,89 @@ export function test(): boolean {
 		["Cherry Pie", "Dumplings"],
 		["Savings", "Careful Accounting"],
 	];
-	const cardDays = [3, 5, 6, 9, 12, 15];
 	const chosenCards = [
 		"Ice Cream",
 		"Affordable",
 		"Christmas Crackers",
 		"Turkey - Stuffing",
 		"Dumplings",
-	].flatMap((name) => Unlocks.filter((a) => a.Name === name));
+	];
 	const startingCards = [
 		RestaurantSettings.filter((a) => a.Name === "Community")[0],
 		Unlocks.filter((a) => a.Name === "Turkey")[0],
 	];
-	a.cards = startingCards;
+	let ok = true;
+	ok &&= testCards(seed, startingCards, expectedCards, chosenCards);
+	ok &&= testCards(
+		"azolmpfb",
+		[
+			RestaurantSettings.filter((a) => a.Name === "Turbo")[0],
+			StartingDishes.filter((a) => a.Name === "Spaghetti")[0],
+		],
+		[
+			["Roast Potato", "All You Can Eat"],
+			["Ice Cream", "Medium Groups"],
+			["Cheesy Spaghetti", "Instant Service"],
+			["Onion Rings", "All You Can Eat"],
+			["Charming", "Affordable"],
+			["Chips", "High Expectations"],
+			["Cheesy Spaghetti", "Simplicity"],
+			["Corn on the Cob", "Tipping Culture"],
+			["Onion Rings", "Tipping Culture"],
+			["Mashed Potato", "Tipping Culture"],
+			["Pumpkin Seed", "Photographic Memory"],
+			["Apple Pies", "High Quality"],
+			["Mandarin Starter", "High Standards"],
+			["Cherry Pie", "Victorian Standards"],
+		],
+		[
+			"Roast Potato",
+			"Ice Cream",
+			"Instant Service",
+			"All You Can Eat",
+			"Affordable",
+			"Chips",
+			"Simplicity",
+			"Corn on the Cob",
+			"Onion Rings",
+			"Mashed Potato",
+			"Photographic Memory",
+			"High Quality",
+			"High Standards",
+			"Victorian Standards",
+		],
+		turboCardDays
+	);
+	return true;
+}
+
+function testCards(
+	seed: string,
+	startingCards: Unlock[],
+	expectedCards: [string, string][],
+	chosenCards: string[],
+	cardDays = normalCardDays
+): boolean {
+	const unlocks = new FindNewUnlocks(seed);
+	for (const card of startingCards) {
+		unlocks.addCard(card);
+	}
 	for (let dayIndex = 0; dayIndex < cardDays.length; dayIndex++) {
 		const day = cardDays[dayIndex];
-		const options = a.getUnlockOptions(day);
+		if (day === 7) debugger;
+		const options = unlocks.getUnlockOptions(day);
 		if (
 			options[0].Name !== expectedCards[dayIndex][0] ||
 			options[1].Name !== expectedCards[dayIndex][1]
 		) {
-			console.log(
+			console.error(
 				`Test failed on day ${day}. Expected ${expectedCards[dayIndex][0]} and ${expectedCards[dayIndex][1]}, but simulated ${options[0].Name} and ${options[1].Name} instead.`
 			);
+			debugger;
 			return false;
 		}
-		a.addCard(chosenCards[dayIndex]);
+		unlocks.addCard(options.filter((a) => a.Name === chosenCards[dayIndex])[0]);
 	}
-	console.log("test passes");
+	console.log(`${seed} test passes.`);
 	return true;
 }
