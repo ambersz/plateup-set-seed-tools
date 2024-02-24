b219096a062b3cc21ca2eb4fa5eb49250a549032
Alice Zhao <alice@zhaos.dev>
2024-02-23 16:00:49 -0800

    simplify default state handling for persisted state hook

--- src/hooks/usePersistentState.ts
+++ src/hooks/usePersistentState.ts
@@ -1,21 +1,32 @@
-import { StateUpdater, useEffect, useState } from "preact/hooks";
+import { StateUpdater, useCallback, useEffect, useState } from "preact/hooks";
 
-export const usePersistentState = <T>(defaultState: T, ID: string) => {
-	const [state, setState] = useState(defaultState);
-	const [first, setFirst] = useState(true);
+export const usePersistentState = <T>(
+	defaultState: T,
+	ID: string
+): [T, StateUpdater<T>] => {
+	const [state, _setState] = useState<T>();
 	useEffect(() => {
-		const cached = localStorage.getItem(ID);
-		if (cached !== null) {
-			setState(JSON.parse(cached));
+		const stored = localStorage.getItem(ID);
+		if (stored === null) {
+		} else {
+			_setState(JSON.parse(stored));
 		}
-	}, []);
+	}, [defaultState, ID]);
+	const setState = useCallback<StateUpdater<T>>(
+		(v) => {
+			if (typeof v === "function") {
+				_setState((oldState) => {
+					const newState = (v as (prevState: T) => T)(oldState ?? defaultState);
+					localStorage.setItem(ID, JSON.stringify(newState));
+					return newState;
+				});
+			} else {
+				_setState(v);
+				localStorage.setItem(ID, JSON.stringify(v));
+			}
+		},
+		[ID, _setState]
+	);
 
-	useEffect(() => {
-		if (localStorage.getItem(ID) !== null && first) {
-			setFirst(false);
-			return;
-		}
-		localStorage.setItem(ID, JSON.stringify(state));
-	}, [state]);
-	return [state, setState] as [T, StateUpdater<T>];
+	return [state ?? defaultState, setState];
 };
