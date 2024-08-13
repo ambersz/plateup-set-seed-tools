66e554956331d75a0448ad89f76c63a8c423ed3d
Alice Zhao <alice@zhaos.dev>
2024-08-12 19:31:33 -0700

    Updates

---
+++ assets/GhostBlueprints-6iE7bqXE.js
@@ -0,0 +1,1 @@
+import{u as t,g as l}from"./main-j4BqBs3i.js";function s({ghostCount:e=2,bps:n}){return t(l,{children:n.map((h,r)=>t("span",{class:r+e<n.length?"":"ghost",children:h+(r<n.length-1?", ":"")}))})}export{s as G};
