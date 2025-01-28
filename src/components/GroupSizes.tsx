bb138800a668ae50c47a14fec479bee19656e90c
AZ <>
2025-01-28 00:20:11 -0800

    remove day length from group size info pae

--- src/components/GroupSizes.tsx
+++ src/components/GroupSizes.tsx
@@ -1,4 +1,3 @@
-import { useState } from "preact/hooks";
 import { defaultBranchingRerollConfig } from "../branchingRerolls";
 import { usePersistentState } from "../hooks/usePersistentState";
 import { RestaurantSettings } from "../workers/db/unlocks";
@@ -24,7 +23,6 @@ const GroupSizes = () => {
 		1,
 		"RIG_PLAYER_COUNT"
 	);
-	const [calls, setCalls] = useState<number[]>([]);
 	const days = [<></>];
 	const nonSetting = config.cards.filter(
 		(a) => !RestaurantSettings.some((b) => b.Name === a.Name)
@@ -37,42 +35,54 @@ const GroupSizes = () => {
 	const run = new Run(config.seed, startingCards, otherCards);
 	run.playerCount = playerCount;
 	let totalCustomerCount = 0;
+	let cumulativeMoney = run.guessMoney(0);
+
+	let rerollsForPots = 2; // let's say I had to spend two rerolls looking for copy desks and such
+	let flowerPotCosts = 60; // discount desk
+	for (let i = 0; i < rerollsForPots; i++) flowerPotCosts += 10 * (i + 1);
+	flowerPotCosts += (20 + 60 + 20 + 20 + 60 + 20 + 60 + 60 + 60 + 40 + 40) / 2; // tools
+	let cumulativeFlowers = 0;
+	let flowers: number[] = [];
 	for (let i = 1; i <= 15; i++) {
 		const groupSizes = run.getGroupSizes(i);
 		const customers = groupSizes.reduce((a, b) => a + b);
 		totalCustomerCount += customers;
-		const normalGroups = Math.ceil(
-			run.getExpectedCustomers(i) / run.getExpectedGroupSize(i)
-		);
-		const dayLength = run.getDayLength(i);
+		const todayMoney = run.guessMoney(i);
+		cumulativeMoney += todayMoney;
+		{
+			flowers.sort((a, b) => b - a);
+			let discountAdjacent = 3;
+			if (flowers.length > 8) discountAdjacent += 3;
+			for (let j = 0; j < Math.min(flowers.length, discountAdjacent); j++) {
+				flowers[j] = Math.ceil(flowers[j] / 2);
+			}
+			flowerPotCosts += flowers.reduce((a, b) => a + b, 0);
+			console.log(JSON.stringify(flowers));
+			cumulativeFlowers += flowers.length;
+			let remaining = cumulativeMoney - flowerPotCosts;
+			while (remaining > 0) {
+				let cost = (rerollsForPots + 1) * 10; // cost of the next reroll
+				cost += 10; // cost of a blueprint cabinet to hold the flower
+				if (!(rerollsForPots % 8)) {
+					// need a new copying desk for the next flower pots
+					cost += 60;
+				}
+				if (cost > remaining) break;
+				rerollsForPots++;
+				flowers.push(30);
+				flowerPotCosts += cost;
+				remaining -= cost;
+			}
+		}
 		days.push(
 			<tr>
 				<td>{i}</td>
 				<td>{groupSizes.length}</td>
 				<td>{customers}</td>
-				<td>{Math.round(run.guessMoney(i))}</td>
+				<td>{Math.round(todayMoney)}</td>
+				<td>{Math.round(cumulativeMoney)}</td>
+				<td>{cumulativeFlowers + flowers.length}</td>
 				<td>{groupSizes.join(",")}</td>
-				<td>
-					<input
-						type="number"
-						style="width:3lh;text-align:center"
-						value={calls[i - 1]}
-						onChange={(e) => {
-							setCalls((calls) => {
-								let copy = [...calls];
-								copy[i - 1] = Number((e.target as HTMLInputElement).value);
-								return copy;
-							});
-						}}
-					/>
-				</td>
-				<td>
-					{(
-						((normalGroups - (calls[i - 1] ?? 0)) / normalGroups) *
-						dayLength
-					).toFixed(0)}{" "}
-					seconds
-				</td>
 			</tr>
 		);
 	}
@@ -97,10 +107,10 @@ const GroupSizes = () => {
 						<td>Day</td>
 						<td>Groups</td>
 						<td>Customers</td>
-						<td>Money?</td>
+						<td>Money</td>
+						<td>Cumulative Money</td>
+						<td>Cumulative Flower Pots</td>
 						<td>All Groups</td>
-						<td>Calls (excluding rushes)</td>
-						<td>Remaining Time</td>
 					</tr>
 				</thead>
 				<tbody>{days}</tbody>
