f52a0cc96bf83e37eed59d62d7fbc98eb1caa66a
AZ <>
2025-01-29 18:40:32 -0800

    usePersistentSignal

--- src/hooks/usePersistentState.ts
+++ src/hooks/usePersistentState.ts
@@ -1,5 +1,34 @@
+import { useSignal, useSignalEffect } from "@preact/signals";
 import { StateUpdater, useCallback, useEffect, useState } from "preact/hooks";
 
+export const usePersistentSignal = <T>(
+	defaultState: T,
+	ID: string,
+	versionUpgrader?: (stored: T) => T
+) => {
+	const signal = useSignal<T>(defaultState);
+	useSignalEffect(() => {
+		console.log(`loading persistentsignal for:`, { defaultState, ID });
+		const stored = localStorage.getItem(ID);
+		if (stored === null) {
+		} else {
+			if (versionUpgrader) {
+				signal.value = versionUpgrader(JSON.parse(stored));
+			} else {
+				signal.value = JSON.parse(stored);
+			}
+		}
+	});
+	useSignalEffect(() => {
+		console.log("saving persistentsignal state");
+		// if (signal.value === undefined) {
+		// 	console.log("probably initial state, skip save");
+		// 	return;
+		// }
+		localStorage.setItem(ID, JSON.stringify(signal.value));
+	});
+	return signal;
+};
 export const usePersistentState = <T>(
 	defaultState: T,
 	ID: string,
